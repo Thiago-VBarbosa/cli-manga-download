835489dceefad0899ecffe81ced9d4cea7297beb
Thiago-VBarbosa <thiago-vbarbosa@hotmail.com>
2022-05-03 16:46:08 -0400

    Inserção da pasta onde serão inseridos os arquivos do SQLite e o banco de dados

---
+++ db/create_base.sql
@@ -0,0 +1,3 @@
+CREATE TABLE list_mangas(nome VARCHAR(30) NOT NULL, cod VARCHAR(40) NOT NULL,
+                    title VARCHAR(60) NOT NULL, autor VARCHAR(30) NOT NULL,
+                    arte VARCHAR(30) NOT NULL, ano int NOT NULL);
