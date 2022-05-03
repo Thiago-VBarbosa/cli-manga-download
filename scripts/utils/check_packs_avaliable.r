09830b7f2bb233707ef68835b9596011e2b85ec5
Thiago-VBarbosa <thiago-vbarbosa@hotmail.com>
2022-05-03 16:50:39 -0400

    Inserção dos pacotes adicionais utilizados

--- scripts/utils/check_packs_avaliable.r
+++ scripts/utils/check_packs_avaliable.r
@@ -1,7 +1,6 @@
-list_packages <- c("xml2","httr","fs",
-    "magick","magrittr","tibble", "glue",
-    "dplyr","stringr","jsonlite",
-    "bookdown","zip","cliapp","docopt")
+list_packages <- c("xml2","httr","fs","magick","magrittr","tibble", "glue",
+    "dplyr","stringr","jsonlite","bookdown","zip","cliapp","docopt",
+    "purrr", "DBI", "RSQLite")
 
 install_missing_packages <- function(pack) {
     if(!(pack %in% rownames(installed.packages()))){
